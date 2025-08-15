92ba230ff9937c88796cbeb1f44d8b5d099486e9
JOYOUNGMIN <jym0421@ciel.co.kr>
2025-08-16 00:54:57 +0900

    feat: highlight text 컴포넌트 추가

---
+++ src/shared/ui/HighlightText.tsx
@@ -0,0 +1,19 @@
+// 텍스트 하이라이트
+export const HighlightText = (text: string, highlight: string) => {
+  if (!text) {
+    return null;
+  }
+
+  if (!highlight.trim()) {
+    return <span>{text}</span>;
+  }
+
+  const regex = new RegExp(`(${highlight})`, "gi");
+  const parts = text.split(regex);
+
+  return (
+    <span>
+      {parts.map((part, i) => (regex.test(part) ? <mark key={i}>{part}</mark> : <span key={i}>{part}</span>))}
+    </span>
+  );
+};
